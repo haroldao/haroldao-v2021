016c10ca3383eb7804345e42a9915f21b5d1119f
haroldao <36776278+haroldao@users.noreply.github.com>
2021-04-06 03:58:05 +0200

    Update

--- .eleventy.js
+++ .eleventy.js
@@ -35,7 +35,8 @@ module.exports = function(eleventyConfig) {
   });
   // Copy Static Files to /_Site
   eleventyConfig.addPassthroughCopy({
-    "./src/admin/config.yml": "./admin/config.yml"
+    "./src/admin/config.yml": "./admin/config.yml",
+    "./src/admin/index.html": "./admin/index.html"
   });
 
   return {
