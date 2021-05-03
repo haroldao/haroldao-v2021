c03d873d1b924f94c10a0a06e9d75a6312accd1a
Harold AO <36776278+haroldao@users.noreply.github.com>
2021-05-04 00:20:10 +0200

    page loader (testing 👀)

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,9 @@ const lscroll = new LocomotiveScroll({
 });
 
 window.addEventListener('load', () =>{
+    setTimeout(() => {
+        document.body.classList.remove('loading');
+    }, 2000);
     console.log("%cThis site was designed and developed by Harold AO in April of 2021. It utilizes Eleventy, Liquid, vanilla javascript, Locomotive Scroll and Splitting.", "color:yellow");
 
     const clamp = (num, min, max) => num <= min ? min : num >= max ? max : num; // Clamps a value between an upper and lower bound
