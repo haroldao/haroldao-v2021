9f7bd60beb2d152d79299b4c72ca5f878326a0a4
Harold AO <36776278+haroldao@users.noreply.github.com>
2021-06-12 20:15:39 +0200

    Console error fixed

--- src/js/time.js
+++ src/js/time.js
@@ -2,10 +2,19 @@ import { DateTime } from "luxon";
 
 window.addEventListener('load', () =>{
     // @see https://github.com/moment/luxon
-    setInterval(() => {
-        const time = document.querySelector(".locale__time output");
-        const timezone = time.getAttribute("data-timezone");
-        const nowTime = DateTime.now().setZone(timezone);
-        time.innerHTML = nowTime.toFormat("HH:mm:ss");
-    }, 1000);
+    
+    const time = document.querySelector(".locale__time output");
+    if (time !== null) {
+        const checkAttribute = time.hasAttribute("data-timezone");
+        if (checkAttribute) {
+            setInterval(() => {
+                    const timezone = time.getAttribute("data-timezone");
+                    const nowTime = DateTime.now().setZone(timezone);
+                    time.innerHTML = nowTime.toFormat("HH:mm:ss");  
+                }, 1000);
+        } 
+        else {
+            return;
+        }
+    }
 });
